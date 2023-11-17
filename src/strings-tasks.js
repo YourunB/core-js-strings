66dda8c44d20f546d3ac0772d5fc25ad671704ec
YourunB <bxyura@gmail.com>
2023-11-17 09:19:21 +0300

    feat: complete removeFirstOccurrences func

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -150,8 +150,11 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  let res = '';
+  if (str.indexOf(value) !== -1) res = str.replace(value, '');
+  else res = str;
+  return res;
 }
 
 /**
