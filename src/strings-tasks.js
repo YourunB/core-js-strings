a45cf7e7bfdd13f4479019bd6dfcf041788625e2
YourunB <bxyura@gmail.com>
2023-11-17 11:11:36 +0300

    feat: complete reverseWords func

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -369,8 +369,12 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const arr = str.split(' ');
+  for (let i = 0; i < arr.length; i += 1) {
+    arr[i] = arr[i].split('').reverse().join('');
+  }
+  return arr.join(' ');
 }
 
 /**
